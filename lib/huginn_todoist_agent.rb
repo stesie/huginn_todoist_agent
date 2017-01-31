8720e4fdce2e314170505ac1bf5949e029d81be4
Stefan Siegl <stesie@brokenpipe.de>
2017-01-31 18:18:50 +0100

    Require 'todoist'

--- lib/huginn_todoist_agent.rb
+++ lib/huginn_todoist_agent.rb
@@ -1,4 +1,11 @@
 require 'huginn_agent'
 
+# Load todoist (from ruby-todoist-api Gem), ignoring LoadErrors.  This way Huginn
+# still starts up successfully, yet shows "Missing Gems" error in the frontend.
+begin
+  require 'todoist'
+rescue LoadError
+end
+
 #HuginnAgent.load 'huginn_todoist_agent/concerns/my_agent_concern'
 HuginnAgent.register 'huginn_todoist_agent/todoist_agent'
