9efca6d4ec0445c1250a9d4cba4777f55bfa22a4
Stefan Siegl <stesie@brokenpipe.de>
2017-02-01 17:38:52 +0100

    first rspec test

--- spec/todoist_agent_spec.rb
+++ spec/todoist_agent_spec.rb
@@ -1,13 +1,52 @@
 require 'rails_helper'
 require 'huginn_agent/spec_helper'
+require 'uri'
 
 describe Agents::TodoistAgent do
   before(:each) do
-    @valid_options = Agents::TodoistAgent.new.default_options
+    @valid_options = {
+      'token' => 'some_token_here',
+      'content' => 'foobar',
+    }
     @checker = Agents::TodoistAgent.new(:name => "TodoistAgent", :options => @valid_options)
     @checker.user = users(:bob)
     @checker.save!
+
+    @event = Event.new
+    @event.agent = agents(:bob_weather_agent)
+    @event.payload = {
+      'somekey' => 'somevalue',
+    }
   end
 
-  pending "add specs here"
+  it 'can create a new static item' do
+    stub_request(:post, "https://todoist.com/API/v6/sync").
+      to_return { |request|
+	expect(request.headers["Content-Type"]).to eq("application/x-www-form-urlencoded")
+
+	form_data = URI.decode_www_form(request.body)
+	expect(form_data.assoc("token").last).to eq("some_token_here")
+
+	json_data = ActiveSupport::JSON.decode(form_data.assoc("commands").last)
+	expect(json_data.length).to eq(1)
+	expect(json_data[0]["type"]).to eq("item_add")
+	expect(json_data[0]["args"]["content"]).to eq("foobar")
+
+	json_response = {
+	  "TempIdMapping" => {
+	    json_data[0]["temp_id"] => 81662555
+	  },
+	  "seq_no_global" => 11248873939,
+	  "seq_no" => 11248873939,
+	  "UserId" => 9933517,
+	  "SyncStatus" => {
+	    json_data[0]["uuid"] => "oK"
+	  },
+	}
+
+	{ status: 200, body: ActiveSupport::JSON.encode(json_response), headers: { "Content-type" => "application/json" } }
+      }
+
+    @checker.receive([@event])
+  end
 end
